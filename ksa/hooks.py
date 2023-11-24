138fb955272a9716d484d77eb7924f00270cc179
Satyabrata Panda <110171620+Satya8848@users.noreply.github.com>
2023-11-24 23:26:11 +0530

    Update hooks.py
    Fix For Vat Setting Already Exist Issue

--- ksa/hooks.py
+++ ksa/hooks.py
@@ -105,7 +105,7 @@ after_install = "ksa.saudi_arabia.setup.setup"
 doc_events = {
 	"Company": {
                 "on_trash": ["ksa.saudi_arabia.utils.delete_vat_settings_for_company"],
-                "on_update": ["ksa.saudi_arabia.wizard.operations.setup_tax_templates.setup_templates",
+                "after_insert": ["ksa.saudi_arabia.wizard.operations.setup_tax_templates.setup_templates",
                               "ksa.saudi_arabia.setup.create_company_settings"]
         },
         "Sales Invoice": {
